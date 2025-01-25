0dcdf61f2188f69d68963a493f888240a9a01de0
Cormac-OK <cormaco@nycstudents.net>
2025-01-25 21:30:52 +0000

    Improved documentation

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -12,8 +12,8 @@ public class BookStore{
      * The users of this bookstore. A static array.
      */
     User[] users = new User[10];
-    //requires 1 empty constructor
 
+    //requires 1 empty constructor
     public BookStore(){}
 
     public User[] getUsers() {
@@ -51,25 +51,28 @@ public class BookStore{
      * @param user The user to remove
      */
     public void removeUser(User user){
+        // Iterates over users, and sets any references to user to null
         for(int i = 0; i < users.length; i ++){
             if (users[i] == user){
                 users[i] = null;
             }
         }
 
+        // Calls consolidateUsers() to shift all users after user down by one index
         consolidateUsers();
     }
     /**
      * Moves all non-null users toward the start of users, and moves all nulls toward the end of users
      */
     public void consolidateUsers(){
-        User[] newArray = new User[users.length];
-        int i = 0;
+        User[] newArray = new User[users.length]; // A new array that users will eventually be set to
 
-        for (User user: users){
+        // iterates over the original user list, adding each non null user to the first empty slot in the new array
+        int placementIndex = 0;
+        for (User user: users){ 
             if (user != null){
-                newArray[i] = user;
-                i++;
+                newArray[placementIndex] = user;
+                placementIndex++;
             }
         }
 
@@ -80,10 +83,15 @@ public class BookStore{
      * @param book The book object to add.
      */
     public void addBook(Book book){
+        // Creates a new Book[] array that books will eventually be set to, with the correct new length
         Book[] newArray = new Book[books.length + 1];
+
+        // Copies all of books to the new array
         for (int i = 0; i < books.length; i ++){
             newArray[i] = books[i];
         }
+
+        // Sets the last item in the new array to
         newArray[books.length] = book;
         
         books = newArray;
@@ -96,11 +104,18 @@ public class BookStore{
      * @param index The index at which to insert book.
      */
     public void insertBook(Book book, int index){
+        // Creates a new Book[] array that books will eventually be set to, with the correct new length
         Book[] newArray = new Book[books.length + 1];
+
+        // Adds all the books before index to the new array
         for (int i = 0; i < index; i ++){
             newArray[i] = books[i];
         }
+
+        // sets the item at index index of the new array to book
         newArray[index] = book;
+
+        // Adds all the books after index to the new array
         for (int i = index + 1; i < newArray.length; i++){
             newArray[i] = books[i-1];
         }
@@ -109,19 +124,30 @@ public class BookStore{
     }
 
 
+    /**
+     * Decreases the quantity of Book book by 1, and then if that quantity is now zero, removes book from books.
+     * @param book The book to remove
+     */
     public void removeBook(Book book){
-        int indexToRemove = -1;
-        for (int i = 0; i < books.length; i ++){
-            if(books[i] == book){
-                indexToRemove = i;
+
+        book.setQuantity(book.getQuantity()-1); // Decreases the quantity of book by one
+
+        
+        // The following section is to remove book from books if its quantity is zero.
+        if (book.getQuantity() == 0){
+
+            // Finding the index in books of book:
+            int indexToRemove = -1;
+            for (int i = 0; i < books.length; i ++){
+                if(books[i] == book){
+                    indexToRemove = i;
+                }
             }
-        }
 
-        books[indexToRemove].setQuantity(books[indexToRemove].getQuantity()-1);
-
-        if (books[indexToRemove].getQuantity() == 0){
+            // Creating a new array of the correct length to replace books
             Book[] newArray = new Book[books.length -1];
 
+            // Iterating over the new book array, adding only the books that have not been modified
             int placementIndex = 0;
             for (int i = 0; i < books.length; i++){
                 if (i != indexToRemove){
@@ -129,20 +155,27 @@ public class BookStore{
                     placementIndex ++;
                 }
             }
+
             books = newArray;
         }
 
     }
        
-    public String bookStoreBookInfo(){
+    /**
+     * @return A list of the info of all the books in books
+     */
+    public String bookStoreBookInfo(){ //you are not tested on this method but use it for debugging purposes
         String str = "";
         for(Book book: books){
             str += book.bookInfo() + "\n";
         }
         return str;
-    } //you are not tested on this method but use it for debugging purposes
+    }
 
-    public String bookStoreUserInfo(){
+    /**
+     * @return A list of the info of all the users in users
+     */
+    public String bookStoreUserInfo(){ //you are not tested on this method but use it for debugging purposes
         String str = "";
         for(User user: users){
             if(user != null){
@@ -153,6 +186,6 @@ public class BookStore{
             }
         }
         return str;
-    } //you are not tested on this method but use it for debugging purposes
+    }
 
 }
